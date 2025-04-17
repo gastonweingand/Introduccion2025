a6939b2ddc4179da656960d1fd6a3f33d811edaa
gastonweingand@hotmail.com <gastonweingand@hotmail.com>
2025-04-17 11:09:21 -0300

    Cambios forms

---
+++ WindowsApp/WindowsApp/Principal.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsApp
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(56, 53);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(183, 26);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.IndianRed;
+            this.btnMostrar.Location = new System.Drawing.Point(28, 114);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(239, 66);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar Nombre Mayúscula";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(855, 479);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtNombre);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Principal";
+            this.Text = "Principal";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Principal_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
---
+++ WindowsApp/WindowsApp/Principal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event handler
+        /// </summary>
+        /// <param name="sender">Control que creo el evento</param>
+        /// <param name="e">argumentos que vienen predefinidos</param>
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mostrando texto en may");
+            MessageBox.Show(txtNombre.Text.ToUpper(), "ADVERTENCIA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Iniciando la aplicación");
+        }
+
+        private void Principal_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = e.X + " " + e.Y;
+        }
+    }
+}
